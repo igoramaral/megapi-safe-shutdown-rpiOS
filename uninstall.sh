bd8f1b54a7ea28fcfd91203f1a16531207c33a8b
Igor Amaral <9051547+igoramaral@users.noreply.github.com>
2026-01-29 11:35:05 -0300

    added uninstall script

---
+++ uninstall.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+set -e
+
+# OVERLAY_NAME="megapi_pw_io"
+# BOOT_CONFIG="/boot/config.txt"
+# OVERLAY_PATH="/boot/overlays/${OVERLAY_NAME}.dtbo"
+SERVICE_NAME="megapi-safe-shutdown"
+SERVICE_FILE="/etc/systemd/system/${SERVICE_NAME}.service"
+
+echo "Uninstalling MegaPi Safe Shutdown..."
+
+# 1. Desativar o serviço
+if systemctl list-unit-files | grep -q "${SERVICE_NAME}"; then
+    echo "Stopping and disabling service..."
+    systemctl stop "${SERVICE_NAME}" || true
+    systemctl disable "${SERVICE_NAME}" || true
+fi
+
+# 2️. Remover o service file
+if [ -f "$SERVICE_FILE" ]; then
+    echo "Removing systemd service file..."
+    rm "$SERVICE_FILE"
+    systemctl daemon-reload
+fi
+
+# 3️. Remover dtoverlay do config.txt
+# if grep -q "$OVERLAY_NAME" "$BOOT_CONFIG"; then
+#     echo "Removing dtoverlay from config.txt..."
+#     sed -i "/dtoverlay=${OVERLAY_NAME}/d" "$BOOT_CONFIG"
+# fi
+
+# 4️⃣ Remover o dtbo
+# if [ -f "$OVERLAY_PATH" ]; then
+#     echo "Removing device tree overlay..."
+#     rm "$OVERLAY_PATH"
+# fi
+
+echo "Uninstall completed."
+# echo "Reboot is required to fully apply changes."
